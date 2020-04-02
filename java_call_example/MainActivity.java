064c136ac12d7af38e6ed99946668bfd810dd089
ECallihan <47329280+ECallihan@users.noreply.github.com>
2020-04-02 14:31:40 -0400

    Add files via upload

--- java_call_example/MainActivity.java
+++ java_call_example/MainActivity.java
@@ -1,54 +1,56 @@
-package com.example.flutterjavav8;
-
-
+package com.example.mailapp;
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.plugin.common.MethodChannel;
-
-import android.annotation.TargetApi;
-import android.content.ContextWrapper;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.PowerManager;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
-import android.os.Bundle;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import android.util.Log;
 
 public class MainActivity extends FlutterActivity {
   private static final String CHANNEL = "heartbeat.fritz.ai/native";
+  private Trial trial = new Trial();
+  private User user;
 
-  @NonNull
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
     GeneratedPluginRegistrant.registerWith(flutterEngine);
 
+
+
     new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
             .setMethodCallHandler((call, result) -> {
-              if (call.method.equals("powerManage")) {
-                boolean deviceStatus = getDeviceStatus();
+                        Thread thread = new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (call.method.equals("parTest")) {
+                                    result.success(trial.parTest(call.argument("email"),call.argument("password")));
+                                }
+                                if (call.method.equals("testMail")) {
+                                    try {
+                                        Log.d("INFO", "inside the Main activity");
+                                        user = new User(call.argument("email"), call.argument("password"));
 
-                String myMessage =  Boolean.toString(deviceStatus);
-                result.success(myMessage);
-
-
-              }
-
-            });
+                                        if(user.getMail())
+                                        {
+                                            result.success("get mail successful");
+                                        }
+                                        else {
+                                            result.success(user.getFileName() + "\nemail: "
+                                                    + call.argument("email") + "\npassword: "
+                                                    + call.argument("password"));
+                                        }
+                                    }
+                                    catch (Exception e)
+                                    {
+                                            Log.d("INFO", "Tried to call again");
+                                    }
+                                }
+                            }
+                        });
+                        thread.start();
+            }
+            );
   }
 
-  @TargetApi(VERSION_CODES.M)
-  private boolean getDeviceStatus() {
-    boolean deviceStatus = false;
-    if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-      PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
-      deviceStatus = powerManager.isDeviceIdleMode();
-
-    }
-
-    return deviceStatus;
-
-  }
 }
--- java_call_example/main.dart
+++ java_call_example/main.dart
@@ -1,54 +1,182 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:async';
 
-void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(
-      primaryColor: Colors.teal,
-      fontFamily: 'Lato',
-      textTheme: ThemeData.light().textTheme.copyWith(
-        title: TextStyle(
-          fontSize: 20,
-          fontFamily: 'Lato',
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    ),
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Testing Java Calls',
-          style: TextStyle(
-            color: Colors.teal,
-          ),
-        ),
-        backgroundColor: Colors.white,
-      ),
-      body: NativeStuff(),
-    ),
-  ));
-}
+void main() => runApp(new MyApp());
 
-class NativeStuff extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  NativeStuffState createState() {
-    return NativeStuffState();
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: new LoginScreen(),
+    );
   }
 }
 
-class NativeStuffState extends State<NativeStuff> {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+/*
+Fields for the login screen:
+-style for text.
+-decoration for input on email and password.
+-controller for email and password.
+ */
+
+  final TextStyle style = TextStyle(
+    fontFamily: 'Montserrat',
+    fontSize: 20.0,
+  );
+  final InputDecoration emailDecor = InputDecoration(
+      contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+      hintText: "Email",
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)));
+  final InputDecoration passwordDecor = InputDecoration(
+      contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+      hintText: "Password",
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)));
+  var _email = new TextEditingController();
+  var _password = new TextEditingController();
+
   static const platformMethodChannel =
-  const MethodChannel('heartbeat.fritz.ai/native');
+      const MethodChannel('heartbeat.fritz.ai/native');
+  String nativeMessage = 'it didn\'t work';
+
+  //Future<Null>
+
+  Future<void> callTest() async {
+    try {
+      nativeMessage = await platformMethodChannel.invokeMethod(
+          "testMail", {"email": _email.text, "password": _password.text});
+    } on PlatformException catch (e) {
+      nativeMessage = "Can't do native stuff ${e.message}.";
+    }
+//    setState(() {
+//
+//      nativeMessage = _message;
+//    }
+//    );
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Login:"),
+      ),
+      body: new ListView(
+        children: <Widget>[
+          new ListTile(
+            title: new TextField(
+              controller: _email,
+              obscureText: false,
+              style: style,
+              decoration: emailDecor,
+            ),
+          ),
+          new ListTile(
+            title: new TextField(
+              controller: _password,
+              obscureText: true,
+              style: style,
+              decoration: passwordDecor,
+            ),
+          ),
+          new ListTile(
+            title: new Material(
+              elevation: 5.0,
+              borderRadius: BorderRadius.circular(30.0),
+              color: Colors.yellow,
+              child: MaterialButton(
+                minWidth: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                onPressed: () {
+                  callTest();
+                  var route = new MaterialPageRoute(
+                    builder: (BuildContext context) => new TestPage(
+                      message: nativeMessage,
+                    ),
+                  );
+                  Navigator.of(context).push(route);
+                },
+                child: Text("Login",
+                    textAlign: TextAlign.center,
+                    style: style.copyWith(
+                        color: Colors.brown, fontWeight: FontWeight.bold)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _textController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Home Page"),
+      ),
+      body: new ListView(
+        children: <Widget>[
+          new ListTile(
+            title: new TextField(
+              controller: _textController,
+            ),
+          ),
+          new ListTile(
+            title: new RaisedButton(
+                child: new Text("Next"),
+                onPressed: () {
+                  var route = new MaterialPageRoute(
+                    builder: (BuildContext context) => new NextPage(
+                      value: _textController.text,
+                    ),
+                  );
+                  Navigator.of(context).push(route);
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyWelcomePage extends StatefulWidget {
+  final String email;
+  final String password;
+  MyWelcomePage({Key key, this.email, this.password}) : super(key: key);
+  @override
+  _MyWelcomePageState createState() => _MyWelcomePageState();
+}
+
+class _MyWelcomePageState extends State<MyWelcomePage> {
+  static const platformMethodChannel =
+      const MethodChannel('heartbeat.fritz.ai/native');
   String nativeMessage = '';
 
-
-  Future<Null> _managePower() async {
+  Future<Null> _accessMail() async {
     String _message;
     try {
-      final String result =
-      await platformMethodChannel.invokeMethod('powerManage');
+      final String result = await platformMethodChannel.invokeMethod("parTest",
+          {"email": "${widget.email}", "password": "${widget.password}"});
       _message = result;
     } on PlatformException catch (e) {
       _message = "Can't do native stuff ${e.message}.";
@@ -60,40 +188,50 @@ class NativeStuffState extends State<NativeStuff> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.teal,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 102.0),
-            child: Center(
-              child: FlatButton.icon(
-                icon: Icon(
-                  Icons.power_settings_new,
-                  size: 200,
-                ),
-                label: Text(''),
-                textColor: Colors.white,
-                onPressed: _managePower,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 102.0),
-            child: Center(
-              child: Text(
-                nativeMessage,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 23.0),
-              ),
-            ),
-          )
-        ],
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Welcome"),
       ),
+      body: new Text(nativeMessage),
     );
   }
+}
 
+class TestPage extends StatefulWidget {
+  final String message;
+  TestPage({Key key, this.message}) : super(key: key);
+  @override
+  _TestPageState createState() => _TestPageState();
+}
+
+class _TestPageState extends State<TestPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Welcome"),
+      ),
+      body: new Text(widget.message),
+    );
+  }
+}
+
+class NextPage extends StatefulWidget {
+  final String value;
+
+  NextPage({Key key, this.value}) : super(key: key);
+  @override
+  _NextPageState createState() => _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Next Page"),
+      ),
+      body: new Text("${widget.value}"),
+    );
+  }
 }
