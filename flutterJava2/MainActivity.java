67a7ddbcb2d92121bd137bb83352e15251b587bf
ECallihan <47329280+ECallihan@users.noreply.github.com>
2020-04-02 14:33:57 -0400

    Add files via upload

--- flutterJava2/MainActivity.java
+++ flutterJava2/MainActivity.java
@@ -5,6 +5,7 @@ import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import android.util.Log;
 
 public class MainActivity extends FlutterActivity {
   private static final String CHANNEL = "heartbeat.fritz.ai/native";
@@ -15,30 +16,41 @@ public class MainActivity extends FlutterActivity {
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
     GeneratedPluginRegistrant.registerWith(flutterEngine);
 
+
+
     new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
             .setMethodCallHandler((call, result) -> {
-              if (call.method.equals("parTest")) {
-                result.success(trial.parTest(call.argument("email"),call.argument("password")));
-              }
-              if (call.method.equals("testMail")) {
-                  try {
-                      user = new User(call.argument("email"), call.argument("password"));
-                      if(user.getMail())
-                      {
-                          result.success("get mail successful");
-                      }
-                      else {
-                          result.success(user.getFileName() + "\nemail: " + call.argument("email"));
-                      }
-                  }
-                  catch (Exception e)
-                  {
-                      result.success("didn't get it");
-                      e.printStackTrace();
-                  }
-              }
+                        Thread thread = new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (call.method.equals("parTest")) {
+                                    result.success(trial.parTest(call.argument("email"),call.argument("password")));
+                                }
+                                if (call.method.equals("testMail")) {
+                                    try {
+                                        Log.d("INFO", "inside the Main activity");
+                                        user = new User(call.argument("email"), call.argument("password"));
+
+                                        if(user.getMail())
+                                        {
+                                            result.success("get mail successful");
+                                        }
+                                        else {
+                                            result.success(user.getFileName() + "\nemail: "
+                                                    + call.argument("email") + "\npassword: "
+                                                    + call.argument("password"));
+                                        }
+                                    }
+                                    catch (Exception e)
+                                    {
+                                            Log.d("INFO", "Tried to call again");
+                                    }
+                                }
+                            }
+                        });
+                        thread.start();
             }
             );
-
   }
+
 }
